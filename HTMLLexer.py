db8be4acaa4a9c77f3496f305f85cbd2ec06ac54
Kyle Morley <316morley@gmail.com>
2020-09-12 14:14:29 -0500

    Initial commit

---
+++ HTMLLexer.py
@@ -0,0 +1,132 @@
+#Rules for our tokenizer
+
+#strip out all content inside html tags<>
+
+from ply import lex
+import re
+
+class HTMLLexer(object):
+
+    tokens = (
+        'TAG',
+        'TEXT_TAG',
+        'FLOAT',
+        'TIME',
+        'HYPENATED',
+        'ABBREVIATED',
+        'WORD',
+        'WHITESPACE',
+        'PUNCTUATION',
+    )
+
+
+    def t_TAG(self, t):
+        r'\s*<[^>]*>\s*'
+        pass
+
+    def t_TEXT_TAG(self, t):
+        r'(\w*<[^>]*>\w*)+'
+        t.value = re.sub("<[^>]*>", "", t.value)
+        return t
+
+    def t_HYPENATED(self, t):
+        r"(\w+-\w+)+"
+        t.value = re.sub("-*", "", t.value)
+        return t
+
+    def t_FLOAT(self, t):
+        r"[-+]?\d*\.\d+"
+        t.value = str(abs(int(float(t.value))))
+        return t
+
+    def t_TIME(self, t):
+        r"\w+:\w+"
+        t.value = re.sub(":*", "", t.value)
+        return t
+
+
+    #words are consisting of only characters
+    #we convert the word to all lowercase
+    def t_WORD(self, t):
+        r'\w+'
+        t.value = str(t.value).lower()
+        return t
+
+
+    def t_WHITESPACE(self, t):
+        r'\s+'
+        pass
+
+    def t_ABBREVIATED(self, t):
+        r"(\w+.\w+)+"
+        t.value = re.sub(".", "", t.value)
+        return t
+
+    def t_PUNCTUATION(self, t):
+        r'[!"#$%&\'\(\)\*\+,-./:;<=>?@\[\]^_`{|}~\\]'
+        pass
+
+    # Error handling rule
+    def t_error(self,t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    # Build the lexer
+    def build(self,**kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        self.frequency = {}
+
+    def updateFrequency(self, freq):
+        for key in freq:
+            if key in self.frequency:
+                self.frequency[key] = self.frequency[key] + freq[key]
+            else:
+                self.frequency[key] = freq[key]
+
+    def tokenizeFile(self, inputFile, outputFile):
+
+        tokens = []
+
+        with open(inputFile,'rb') as f:
+            for line in f:
+                line = line.decode(errors='ignore')
+                self.lexer.input(line)
+                while True:
+                    tok = self.lexer.token()
+                    if not tok:
+                        break
+                    tokens.append(tok.value)
+
+        with open(outputFile, 'w') as f:
+            for x in tokens:
+                f.write("%s\n" % x)
+
+
+        #Count the frequency of tokens for this file
+        freq = {}
+        for x in tokens:
+            if (x in freq):
+                freq[x] += 1
+            else:
+                freq[x] = 1
+
+        self.updateFrequency(freq)
+
+    def finish(self):
+
+        with open("sorted.txt", "w") as f:
+            for w in sorted(self.frequency):
+                f.write("%s\n" % w)
+
+        with open("frequency.txt", "w") as f:
+            for w in sorted(self.frequency, key=self.frequency.get, reverse=True):
+                f.write("%s %d\n" % (w, self.frequency[w]))
+
+    # Test it output
+    def test(self,data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print(tok)
---
+++ main.py
@@ -0,0 +1,53 @@
+import string
+import sys
+import os
+from HTMLLexer import HTMLLexer
+import shutil
+#a directory of all tokenized documents (one output file per input file)
+#a file of all tokens and their frequencies sorted by token
+#a file of all tokens and their frequencies sorted by frequency
+
+
+if __name__ == "__main__":
+
+    m = HTMLLexer()
+    m.build()
+
+    m.test(" <html>\n <head><title>Another test file</title></head>\n <body> ")
+    directory = os.path.join(os.getcwd(), "files")
+    files = os.listdir(directory)
+
+
+    if os.path.exists('output'):
+        shutil.rmtree('output')
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+
+    for file in files:
+        if file.endswith(".html"): #might have to change to "b.html" on linux systems
+            inputFile = os.path.join(directory,file)
+            outputFile = os.path.join(os.getcwd(), "output/" + str(file)[:-5] + "_tokenized.txt")
+
+            m.tokenizeFile(inputFile, outputFile)
+
+
+    m.finish()
+    #todo build a file that has counts of all the tokens, then build a file that has all the tokens sorted
+
+"""
+    for filename in os.listdir(directory):
+        print(filename)
+        if filename.endswith(".html"):
+            with open(os.path.join(directory, filename), 'r') as f:
+                print("made it")
+
+"""
+
+
+
+
+    #run a tokenizer task
+    #send to a reducer to count the occurrences of each token
+    #sort by both token and frequency
